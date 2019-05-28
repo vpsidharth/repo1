834e45c3e86a26f8d9f3a51c8e236e1b57530397
sidharth.vp <sidharth.vp@nissanmotor.com>
2019-05-28 21:17:44 +0530

    My First Commit

---
+++ AddSubtract.java
@@ -0,0 +1,29 @@
+class AddSubtract
+{
+  //test merge
+public static void main(String[] arg)
+{
+int a,b,c,d;
+Scanner sc=new Scanner(System.in);
+System.out.println("Enter first number");
+a=sc.nextInt();
+System.out.println("Enter second number");
+b=sc.nextInt();
+c=addition(a,b);
+//commit 1 in master
+System.out.println(" Addition of two numbers is : "+c);
+
+d=subtraction(a,b);
+System.out.println(" subtraction of two numbers is : "+d);
+
+}
+static int addition(int x,int y)
+{
+	return x+y;
+}
+static int subtraction(int x,int y)
+{
+  	System.out.println("Commit 2 - master branch);
+	return x-y;
+}
+}
